cea1147d7409a6bf515e675b1662cc3009e4cd6d
NeedGOOD <vladyakusevich@gmail.com>
2024-11-03 00:30:59 +0200

    Installed jwt framework. Created sign in jwt token.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JWT_SECRET } from 'src/configs/jwt-secret';
 
 @Module({
-  imports: [UsersModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '1d' }
+    })
+  ]
 })
 export class AuthModule { }
